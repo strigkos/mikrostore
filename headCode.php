08431a29aa1a00a5debff35b7aca67fb300e2aff
strigkos <31070654+strigkos@users.noreply.github.com>
2017-09-30 22:42:46 +0300

    Add files via upload

---
+++ headCode.php
@@ -0,0 +1,53 @@
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="js/jquery.min.js"></script>
+	<!-- Custom Theme files -->
+	<!--theme-style-->
+	<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />	
+	<!--//theme-style-->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<script type="application/x-javascript">
+		addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+	<!--fonts-->
+	<link href='https://fonts.googleapis.com/css?family=Cabin:400,500,600,700' rel='stylesheet' type='text/css'>
+	<!--//fonts-->
+	<!--//slider-script-->
+	<script>$(document).ready(function(c) {
+		$('.alert-close').on('click', function(c){
+			$('.message').fadeOut('slow', function(c){
+				$('.message').remove();
+			});
+		});
+	});
+	</script>
+	<script>$(document).ready(function(c) {
+		$('.alert-close1').on('click', function(c){
+			$('.message1').fadeOut('slow', function(c){
+				$('.message1').remove();
+			});
+		});
+	});
+	</script>
+	<script>$(document).ready(function(c) {
+		$('.alert-close2').on('click', function(c){
+			$('.message2').fadeOut('slow', function(c){
+				$('.message2').remove();
+			});
+		});
+	});
+	</script>
+	<script type="text/javascript" src="js/move-top.js"></script>
+	<script type="text/javascript" src="js/easing.js"></script>
+	<script type="text/javascript">
+		jQuery(document).ready(function($) {
+			$(".scroll").click(function(event){
+				event.preventDefault();
+				$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+			});
+		});
+	</script>
+	<!-- start menu -->
+	<link href="css/megamenu.css" rel="stylesheet" type="text/css" media="all" />
+	<script type="text/javascript" src="js/megamenu.js"></script>
+	<script>$(document).ready(function(){$(".megamenu").megamenu();});</script>
